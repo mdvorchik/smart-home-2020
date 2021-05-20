fac5fb8b5184e730256bcd5a0b7ffa1d82ade738
Maxim Dvorchik <maxim.dvorchik@yandex.ru>
2021-05-20 15:45:00 +0300

    revert to early commit

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,44 +1,43 @@
 package ru.sbt.mipt.oop;
 
-import com.coolcompany.smarthome.events.SensorEventsManager;
 import ru.sbt.mipt.oop.command.SmartHomeCommandSender;
-import ru.sbt.mipt.oop.event.DoorEventProcessor;
-import ru.sbt.mipt.oop.event.LightEventProcessor;
-import ru.sbt.mipt.oop.event.SecurityEventProcessor;
-import ru.sbt.mipt.oop.event.api.coolcompany.FromEventProcessorToEventHandlerAdaptor;
+import ru.sbt.mipt.oop.event.*;
 import ru.sbt.mipt.oop.house.SmartHome;
 import ru.sbt.mipt.oop.utils.JsonReader;
 import ru.sbt.mipt.oop.utils.Logger;
 import ru.sbt.mipt.oop.utils.LoggerToConsole;
 import ru.sbt.mipt.oop.utils.StateReader;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     private final Logger logger;
     private final StateReader stateReader;
+    private final EventCreator eventCreator;
 
-    public Application(Logger loger, StateReader stateReader){
+    public Application(Logger loger, StateReader stateReader, EventCreator eventCreator){
         this.logger = loger;
         this.stateReader = stateReader;
+        this.eventCreator = eventCreator;
     }
 
     public void run() {
         // считываем состояние дома из файла
         SmartHome smartHome = stateReader.readStateOfHome("smart-home-1.js");
         // создаем список обработчиков возможный событий
-        SensorEventsManager sensorEventsManager = new SensorEventsManager();
-        sensorEventsManager.registerEventHandler(new FromEventProcessorToEventHandlerAdaptor(
-                new SecurityEventProcessor(new DoorEventProcessor(logger, new SmartHomeCommandSender(logger))),
-                smartHome));
-        sensorEventsManager.registerEventHandler(new FromEventProcessorToEventHandlerAdaptor(
-                new SecurityEventProcessor(new LightEventProcessor(logger)),
-                smartHome));
+        List<EventProcessor> eventProcessorList = new ArrayList<>();
+        eventProcessorList.add(new SecurityEventProcessor(new DoorEventProcessor(logger, new SmartHomeCommandSender(logger))));
+        eventProcessorList.add(new SecurityEventProcessor(new LightEventProcessor(logger)));
+        eventProcessorList.add(new AlarmEventProcessor());
         // начинаем цикл обработки событий
-        sensorEventsManager.start();
+        AggregateEventProcessor eventProcessor = new AggregateEventProcessorImpl(logger, eventProcessorList);
+        eventProcessor.processEvents(eventCreator, smartHome);
     }
 
     public static void main(String... args) {
-        Application application = new Application(new LoggerToConsole(), new JsonReader());
+        Application application = new Application(new LoggerToConsole(), new JsonReader(), new EventCreatorImpl());
         application.run();
     }
 }
